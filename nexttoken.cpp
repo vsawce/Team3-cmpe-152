861ff0b73ed1f02ff5923dfc3911582292159047
MichaelXiao9625 <michaelkrakow9625@gmail.com>
2022-09-07 19:49:57 -0700

    nexttoken with working part of using input

--- nexttoken.cpp
+++ nexttoken.cpp
@@ -1,28 +1,5 @@
-//
-//  main.cpp
-//  Assignment2
-//
-//  Created by MSSV on 9/7/22.
-//
-
 #include <iostream>
 using namespace std;
-
-class Scanner
-{
-public:
-    void nexttoken()
-    {
-        return;
-    }
-private:
-    string table[200][2];
-};
-
-int main(int argc, const char * argv[]) {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
 
 string nextToken(istream& in){
     int c;
