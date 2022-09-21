3c1b9450aa6d8938133aced3134d62f6042f4b86
MichaelXiao9625 <michaelkrakow9625@gmail.com>
2022-09-21 09:39:33 -0700

    created and organized tokens, node, and node label

---
+++ Tokens/operator/operator.cpp
@@ -0,0 +1,89 @@
+#include <string>
+#include "operator.h"
+
+const char ADDITION = '+';
+const char SUBTRACTION = '-';
+const char MULTIPLICATION = '*';
+const char DIVISION = '/';
+const char NEGATION = '#';
+const char LESS_THAN = '<';
+const char GREATER_THAN = '>';
+const char ASSIGNMENT = '=';
+const char EQUALS = ':';
+
+bool OperatorToken::is_operator(char character)
+{
+    return operators.find(character) != operators.end();
+}
+
+bool OperatorToken::is_addition_token(Token token)
+{
+    Token addition_token = OperatorToken(ADDITION);
+    return token == addition_token;
+}
+
+bool OperatorToken::is_subtraction_token(Token token)
+{
+    Token subtraction_token = OperatorToken(SUBTRACTION);
+    return token == subtraction_token;
+}
+
+bool OperatorToken::is_multiplication_token(Token token)
+{
+    Token multiplication_token = OperatorToken(MULTIPLICATION);
+    return token == multiplication_token;
+}
+
+bool OperatorToken::is_division_token(Token token)
+{
+    Token division_token = OperatorToken(DIVISION);
+    return token == division_token;
+}
+
+bool OperatorToken::is_negation_token(Token token)
+{
+    Token negation_token = OperatorToken(NEGATION);
+    return token == negation_token;
+}
+
+bool OperatorToken::is_less_than_token(Token token)
+{
+    Token less_than_token = OperatorToken(LESS_THAN);
+    return token == less_than_token;
+}
+
+bool OperatorToken::is_greater_than_token(Token token)
+{
+    Token greater_than_token = OperatorToken(GREATER_THAN);
+    return token == greater_than_token;
+}
+
+bool OperatorToken::is_equals_token(Token token)
+{
+    Token equals_token = OperatorToken(EQUALS);
+    return token == equals_token;
+}
+
+bool OperatorToken::is_assignment_token(Token token)
+{
+    Token assignment_token = OperatorToken(ASSIGNMENT);
+    return token == assignment_token;
+}
+
+OperatorToken::OperatorToken(std::string value, int line_number)
+: Token(OperatorTokenId, value, line_number) {}
+
+OperatorToken::OperatorToken(char value)
+: Token(OperatorTokenId, value) {}
+
+std::set<char> OperatorToken::operators = {
+    ADDITION,
+    SUBTRACTION,
+    MULTIPLICATION,
+    DIVISION,
+    NEGATION,
+    LESS_THAN,
+    GREATER_THAN,
+    EQUALS,
+    ASSIGNMENT
+};
---
+++ src/Parser.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "../inc/Parser.h"
+#include "../inc/scanner.h"
+#include "../Tokens/Token.h" 
+
+// :(
