4dc82b0ae37a4c885a5fb058f192616d6511fae3
vsawce <vsaw30@gmail.com>
2022-09-20 23:46:42 -0700

    Migrated scanner to separate header/source away from main.cpp

--- inc/scanner.h
+++ inc/scanner.h
@@ -1,6 +1,54 @@
 #ifndef SCANNER_H_
 #define SCANNER_H_
 
-void printhello();
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+typedef enum {
+    CHAR_LETTER,        //0
+    CHAR_DIGIT,         //1
+    CHAR_SINGLEQUOTE,   //2
+    CHAR_DECIMAL,       //3
+    CHAR_SPECIAL_SYM,   //4
+    CHAR_WHITESPACE,    //5
+    CHAR_EOF,           //6
+    CHAR_UNKNOWN        //7
+} char_state;
+
+typedef enum {
+    ST_FIRSTCHAR,     //0
+    ST_WORD,          //1
+    ST_OPERATOR,      //2
+    ST_STRING,        //3
+    ST_INTEGER,       //4
+    ST_REAL_NUM,      //5
+    ST_ERROR,         //6
+    ST_END            //7
+} token_state;
+
+class Scanner
+{
+public:
+    Scanner();
+    std::string GetLabel(std::string token) const;
+    std::string GetToken(std::string label) const;
+    void nexttoken()
+    {
+        return;
+    }
+private:
+    ////////////////////
+    // Column 0 - Token
+    // Column 1 - Label
+    ////////////////////
+    std::string table[128][2];
+};
+
+std::string nextToken(Scanner sc, ifstream& testFile, ofstream& out);
 
 #endif /* SCANNER_H_ */
