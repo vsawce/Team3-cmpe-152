1125f058ed475e62d15a2898b934856041820c9b
MichaelXiao9625 <michaelkrakow9625@gmail.com>
2022-09-21 20:35:04 -0700

    updated node labels

--- inc/node_labels.h
+++ inc/node_labels.h
@@ -3,20 +3,20 @@
 
 #include <string>
 
-const std::string START = "S";
+const std::string START = "start";
 const std::string BLOCK = "block";
 const std::string VARS = "vars";
 const std::string EXPR = "expr";
-const std::string HASH = "H";
+const std::string HASH = "hash";
 const std::string R_LETTER = "R";
 const std::string STATS = "stats";
 const std::string M_STAT = "m_stat";
 const std::string STAT = "stat";
-const std::string IN = "in";
+const std::string INPUT = "input";
 const std::string OUT = "out";
 const std::string IFSTAT = "ifstat";
 const std::string LOOP = "loop";
 const std::string ASSIGN = "assign";
-const std::string OPERATOR = "O";
+const std::string OPERATOR = "op";
 
 #endif
