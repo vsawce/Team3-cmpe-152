bb157a9423d8c34d83fe37be7ff53f50fe96c3a4
MichaelXiao9625 <michaelkrakow9625@gmail.com>
2022-09-21 19:25:01 -0700

    updated parser cpp and h

--- inc/Parser.h
+++ inc/Parser.h
@@ -10,6 +10,33 @@
 class Parser
 {
     public:
+     Parser(std::string filename);
+        ~Parser();
+        Node* parse();
+
+    private:
+        Scanner* scanner;
+        Token token;
+
+        Node* start();
+        Node* block(int level);
+        Node* vars(int level);
+        bool is_first_of_stats(Token token);
+        Node* stat(int level);
+        Node* op(int level);
+        bool is_op_token(Token token);
+        Node* expr(int level);
+        Node* hash(int level);
+        Node* R(int level);
+        Node* stats(int level);
+        Node* m_stat(int level);
+        Node* input(int level);
+        Node* out(int level);
+        Node* ifstat(int level);
+        Node* loop(int level);
+        Node* assign(int level);
+        void printError();
+        void checkComma();
 };
 
 #endif
