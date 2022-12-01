0f128516a12656b2c0ccbf148477f48b79b27a45
MichaelXiao9625 <michaelkrakow9625@gmail.com>
2022-11-30 22:19:10 -0800

    added to ExprMain

--- ExprMain.cpp
+++ ExprMain.cpp
@@ -41,7 +41,8 @@ std::string lispToXml(ifstream &insLisp) {
             xmlString += "</" + strStack.back() + ">\n";
             token = "";
             strStack.pop_back();
-            //xmlString += "\n";
+            xmlString += token;
+        } //xmlString += "\n";
         }
         else if (c == ' ') {
             for (int i = 0; i < level; i++) {
@@ -61,8 +62,11 @@ std::string lispToXml(ifstream &insLisp) {
             token += c;
         }
         if (levelChange) {
+            for (int i = 0; i < level; i++) {
+                xmlString += "\t";
+            }
             //xmlString += "\n";
-        }
+        append(xmlString, token);
         //append to list
     }
     return xmlString;
@@ -72,13 +76,42 @@ void lispToSymtab(ifstream &insLisp, ofstream &outsSymtab) {
     char c;
     intermediate::symtab::SymtabStack *sts = new intermediate::symtab::SymtabStack();
     intermediate::symtab::Symtab *st = sts->getLocalSymtab();
+    std::string token = "";
+    bool firstToken = false;
+    list<std::string> strStack;
     //int level = -1;
     //std::string xmlString = "";
     std::string token = "";
     intermediate::symtab::Kind kind;
     bool isType = false;
-    bool nextIsIdentifier = false;
+    /*bool nextIsIdentifier = false;
+    bool nextIsType = false;
+    bool nextIsKind = false;
+    bool nextIsValue = false;
+    bool nextIsRoutine = false;
+    bool nextIsParameter = false;
+    bool nextIsLocal = false;
+    bool nextIsField = false;
+    bool nextIsFunction = false;
+    bool nextIsProcedure = false;
+    bool nextIsVariable = false;
+    bool nextIsConstant = false;
+    bool nextIsTypeSpec = false;
+    bool nextIsRecord = false;
+    bool nextIsArray = false;
+    bool nextIsSubrange = false;
+    bool nextIsEnumeration = false;
+    bool nextIsScalar = false;
+    bool nextIsString = false;
+    bool nextIsInteger = false;
+    bool nextIsReal = false;
+    bool nextIsBoolean = false;
+    bool nextIsChar = false;
+    bool nextIsValueRange = false;
+    bool nextIsValueList = false;
+    bool nextIsValueRangeList = false;*/
     //bool firstToken = false;
+    list<std::string> strStack;
     //list<std::string> strStack;
 
     while (insLisp >> noskipws >> c) { // Not EOF
@@ -122,10 +155,15 @@ void lispToSymtab(ifstream &insLisp, ofstream &outsSymtab) {
         else {
             token += c;
         }
+        append(xmlString, token);
         //append to list
+        cout << xmlString << endl;
         //cout << token << endl;
     }
-    //vector<SymtabEntry *> sortedE = st->sortedEntries();
+    vector<intermediate::symtab::SymtabEntry *> entries = st->sortedEntries();
+    for (intermediate::symtab::SymtabEntry *entry : entries) {
+        outsSymtab << entry->getName() << " " << to_underlying(entry->getKind()) << endl;
+    } //vector<SymtabEntry *> sortedE = st->sortedEntries();
     for (int i = 0; i < st->sortedEntries().size(); i++) { //For each entry
         outsSymtab << i << " " << st->sortedEntries()[i]->getName() << "\t\t" << intermediate::symtab::KIND_STRINGS[to_underlying(st->sortedEntries()[i]->getKind())] << "\t\t"  <<endl;
     }
@@ -141,39 +179,71 @@ ifstream ins;
 ofstream outs(outFile);
 ofstream outsLisp(lispFile);
 ofstream outsXml(xmlFile);
+ofstream outsSymtab("test-out-symtab.txt");
 // Create the input stream.
 ins.open(args[1]);
+if (ins.fail()) {
+    cerr << "***** ERROR: Cannot open source file " << args[1] << endl;
+    exit(-1);
+}
 // outs.open(outFile);
 ANTLRInputStream input(ins);
+// Create an input character stream from standard in
+// ANTLRInputStream input(cin);
+// Create an ExprLexer that feeds from that stream
+ExprLexer lexer(&input);
+// Create a stream of tokens fed by the lexer
+CommonTokenStream tokens(&lexer);
+// Create a parser that feeds off the token stream
+ExprParser parser(&tokens);
+// Create a tree walker that feeds off the parser's tree
+tree::ParseTreeWalker walker;
+//tree::ParseTree *tree = parser.program();
+// Walk the tree created during the parse, trigger callbacks
+// on entry to and exit from each rule.
+//ExprListener listener;
+//walker.walk(&listener, parser.program());
 // Create a lexer which scans the input stream
 // to create a token stream.
-ExprLexer lexer(&input);
 CommonTokenStream tokens(&lexer);
 // Print the token stream.
+//tokens.fill();
+//for (Token *token : tokens.getTokens()) {
+//    cout << token->toString() << endl;
+//}
 outs << "Tokens:" << endl;
 tokens.fill();
 for (Token *token : tokens.getTokens())
 {
 outs << token->toString() << std::endl;
 }
-// Create a parser which parses the token stream
-// to create a parse tree.
-ExprParser parser(&tokens);
-tree::ParseTree *tree = parser.program();
-
 cout << endl << "-----" << endl;
 // Print the parse tree in Lisp format.
-outs << endl << "Parse tree (Lisp format):" << endl;
+outsLisp << "Parse tree:" << endl;
+outsLisp << parser.program()->toStringTree(&parser) << endl;
+cout << endl << "-----" << endl;
+// Print the parse tree in XML format.
+outsXml << "Parse tree:" << endl;
+outsXml << parser.program()->toStringTree(&parser) << endl;
+cout << endl << "-----" << endl;
+// Print the symbol table.
+outsSymtab << "Symbol table:" << endl;
+outsSymtab << parser.program()->toStringTree(&parser) << endl;
+cout << endl << "-----" << endl;
+
+/*outs << endl << "Parse tree (Lisp format):" << endl;
 outs << tree->toStringTree(&parser) << endl;
-outsLisp << tree->toStringTree(&parser) << endl;
+outsLisp << tree->toStringTree(&parser) << endl;*/
 
 ifstream insLisp(lispFile);
 ifstream insLisp2(lispFile);
-
+ifstream insXml(xmlFile);
+//ofstream outsSymtab("test-out-symtab.txt");
 
 outsXml << lispToXml(insLisp) << endl;
 outs << endl;
 lispToSymtab(insLisp2, outs);
+//lispToSymtab(insLisp2, outsSymtab);
 
 cout << "Program complete, check: " << outFile << ", " << lispFile << ", " << xmlFile << endl;
 return 0;
